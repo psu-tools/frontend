71fa17e8f6f9310eb7f219064dbe363d5fab82ca
Eldar Sayranov <sayranoveldar@gmail.com>
2025-03-18 00:28:20 +0500

    #11 Edit trip components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import Navbar from "~/widgets/navbar/Navbar.vue";
-
+import Navbar from '~/widgets/navbar/Navbar.vue'
 </script>
 
 <template>
-  <div class="relative bg-white  sm:w-96 h-screen sm:h-[700px] flex flex-col">
-    <div class="flex-1 overflow-y-auto pt-6 px-4">
+  <div class="relative bg-white sm:w-96 h-screen sm:h-[700px] flex flex-col">
+    <div class="flex-1 overflow-y-auto pt-8 px-4">
       <slot />
     </div>
     <Navbar />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
