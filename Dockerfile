f0723c453f13259d8215181c0f2148dfed555509
Ilya Alikin <iljuha.alikin@gmail.com>
2025-03-22 01:47:03 +0500

    Update Docker files

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:18 AS builder
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+RUN npm install --frozen-lockfile
+
+COPY . .
+
+RUN npx nuxt prepare
+
+RUN npm run build
+
+FROM node:18-alpine
+WORKDIR /app
+
+COPY --from=builder /app/.output .output
+COPY --from=builder /app/package.json package.json
+COPY --from=builder /app/node_modules/.prisma node_modules/.prisma
+
+RUN npm install --only=production
+
+EXPOSE 3000
+
+CMD ["node", ".output/server/index.mjs"]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,33 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [vue(), tailwindcss()],
   },
   app: {
     link: [
       {
         rel: 'preconnect',
-        href: 'https://fonts.googleapis.com'
+        href: 'https://fonts.googleapis.com',
       },
       {
         rel: 'preconnect',
         href: 'https://fonts.gstatic.com',
-        crossorigin: 'anonymous'
+        crossorigin: 'anonymous',
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap'
-      }
-    ]
+        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
+      },
+    ],
   },
   runtimeConfig: {
     public: {
       apiHost: process.env.API_HOST || 'https://api.psu-tools.ru/routes-service',
-    }
-  }
+    },
+  },
 })
