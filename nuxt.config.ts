6cbeaf028504fdcc4c26db1b13110f703e5ec062
Eldar <sayranoveldar@gmail.com>
2025-03-27 10:51:37 +0500

    #24 Add dev server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   vite: {
     plugins: [
       tailwindcss(),
