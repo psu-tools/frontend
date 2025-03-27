e7eafc3b0726b7f69a2f2d61a8cd7ebfced538ba
Eldar <sayranoveldar@gmail.com>
2025-03-27 10:57:52 +0500

    #24 Fix navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@ import Navbar from '~/widgets/navbar/Navbar.vue'
 </script>
 
 <template>
-  <div class="relative bg-white sm:w-96 h-screen sm:h-[700px] flex flex-col">
+  <div class="relative bg-white sm:w-96 h-dvh sm:h-[700px] flex flex-col">
     <div class="flex-1 overflow-y-auto pt-8 px-4">
       <slot />
     </div>
