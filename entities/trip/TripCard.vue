ebfc3cef68368a8873f67bc309a4a9c3cd33f862
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-02 19:40:43 +0500

    #35 Transfer trip card into a single entity

---
+++ entities/trip/TripCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ActiveTripCard from '~/entities/trip/ActiveTripCard.vue'
+import PrimaryTripCard from '~/entities/trip/PrimaryTripCard.vue'
+
+interface TripProps {
+  status: string
+  trip: Trip
+  hasDate: boolean
+}
+
+defineProps<TripProps>()
+</script>
+<template>
+  <div v-if="status === 'active'">
+    <ActiveTripCard :trip="trip" />
+  </div>
+  <div v-else-if="status === 'primary'">
+    <PrimaryTripCard :trip="trip" :has-date="hasDate" />
+  </div>
+</template>
