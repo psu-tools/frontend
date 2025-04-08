71f5060bb3577c5f8760acfa2c041d0998792103
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-08 20:10:06 +0500

    #35 Hide scrollbar

--- pages/tripPopup.vue
+++ pages/tripPopup.vue
@@ -71,7 +71,7 @@ console.log('asdsd', stopsList)
       @click="closeModal"
     >
       <div
-        class="w-full bg-(--primary-white-bg) items-end rounded-t-3xl pt-2 px-5 transition-all duration-300 touch-none overflow-auto"
+        class="w-full bg-(--primary-white-bg) items-end rounded-t-3xl pt-2 px-5 transition-all duration-300 touch-none overflow-auto scrollbar-hide"
         :class="{
           'h-6/10 translate-y-0': !isExpanded,
           'h-9/10 translate-y-0': isExpanded,
@@ -120,3 +120,12 @@ console.log('asdsd', stopsList)
     </div>
   </Teleport>
 </template>
+<style scoped>
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+
+.scrollbar-hide {
+  scrollbar-width: none;
+}
+</style>
