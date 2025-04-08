3d50321acad9a07d316b2560dbac3ab031bdf9cf
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-08 18:23:55 +0500

    #35 Add route line

--- entities/route/RouteBlock.vue
+++ entities/route/RouteBlock.vue
@@ -14,12 +14,19 @@ interface RoutesBlockProps {
 defineProps<RoutesBlockProps>()
 </script>
 <template>
-  <div class="">
-    <div class=""></div>
-    <div class="flex flex-col gap-1.5">
+  <div class="w-full flex items-stretch gap-[15px]">
+    <div class="bg-(--primary-orange) w-[20px] rounded-full z-10 relative">
+      <span
+        class="absolute top-[4px] left-1/2 transform -translate-x-1/2 bg-(--primary-white) w-[12px] h-[12px] rounded-full z-20"
+      ></span>
+      <span
+        class="absolute bottom-[4px] left-1/2 transform -translate-x-1/2 bg-(--primary-white) w-[12px] h-[12px] rounded-full z-20"
+      ></span>
+    </div>
+    <div class="flex flex-col flex-auto gap-1.5">
       <div class="w-full flex justify-between items-center">
         <div class="flex flex-col">
-          <p class="text-[14px] text-(--color-text) font-semibold">
+          <p class="text-[14px] text-(--color-text) font-semibold w-[200px] break-all">
             {{ startingPoint.name }}
           </p>
           <p class="text-[12px] text-(--primary-gray)">Адрес</p>
@@ -29,7 +36,7 @@ defineProps<RoutesBlockProps>()
       <div class="text-[12px] text-(--color-text)">Тип транспорта</div>
       <div class="w-full flex justify-between items-center">
         <div class="flex flex-col">
-          <p class="text-[14px] text-(--color-text) font-semibold">
+          <p class="text-[14px] text-(--color-text) font-semibold w-[200px] break-all">
             {{ arrivalPoint.name }}
           </p>
           <p class="text-[12px] text-(--primary-gray)">Адрес</p>
--- entities/route/RoutesContainer.vue
+++ entities/route/RoutesContainer.vue
@@ -14,14 +14,19 @@ interface Stop {
 defineProps<Routes>()
 
 const stopsList = ref([
-  { name: 'Perm State University', latitude: 58.01045, longitude: 56.229446, stopTime: 15 },
+  {
+    name: 'Perm State Universitniversitnsdsdsdsdiversitdsy',
+    latitude: 58.01045,
+    longitude: 56.229446,
+    stopTime: 15,
+  },
   { name: 'Perm ', latitude: 58.01045, longitude: 56.229446, stopTime: 15 },
   { name: 'Perm State Art Gallery', latitude: 58.016667, longitude: 56.241667, stopTime: 25 },
   { name: 'Деревня', latitude: 58.016667, longitude: 56.241667, stopTime: 25 },
 ])
 </script>
 <template>
-  <div class="flex flex-col gap-[25px]">
+  <div class="relative flex flex-col gap-[25px]">
     <div v-for="(stop, index) in stopsList">
       <RouteBlock
         v-if="index !== stopsList.length - 1"
@@ -29,5 +34,9 @@ const stopsList = ref([
         :arrival-point="stopsList[index + 1]"
       />
     </div>
+    <div
+      class="absolute top-0 left-0 w-[20px] bg-[#eeeeee] rounded-full"
+      style="height: calc(100% - 25px)"
+    ></div>
   </div>
 </template>
