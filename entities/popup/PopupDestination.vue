94a68bc6298b1767192f794e95ae4decb4b095d7
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-08 16:26:34 +0500

    #35 Add stops behaviour

--- entities/popup/PopupDestination.vue
+++ entities/popup/PopupDestination.vue
@@ -1,32 +1,42 @@
 <script setup lang="ts">
-interface DestinationProps {
-  stopsList: Array<Stop>
-}
+// interface DestinationProps {
+//   stopsList: Array<Stop>
+// }
 
-interface Stop {
-  name: String
-  latitude: Number
-  longitude: Number
-  stopTime: Number
-}
+// interface Stop {
+//   name: String
+//   latitude: Number
+//   longitude: Number
+//   stopTime: Number
+// }
 
-defineProps<DestinationProps>()
+const stopsList = ref([
+  { name: 'Perm State University', latitude: 58.01045, longitude: 56.229446, stopTime: 15 },
+  { name: 'Perm State Art Gallery', latitude: 58.016667, longitude: 56.241667, stopTime: 25 },
+  { name: 'Perm State Art Gallery', latitude: 58.016667, longitude: 56.241667, stopTime: 25 },
+  { name: 'Perm State Art Gallery', latitude: 58.016667, longitude: 56.241667, stopTime: 25 },
+])
+
+// defineProps<DestinationProps>()
 </script>
-
 <template>
-  <div class="relative flex flex-col gap-7">
-    <div v-for="(stop, index) in stopsList" :key="index">
-      <div
-        class="relative pl-[30px] before:absolute before:w-3.5 before:h-3.5 before:bg-(--primary-orange) before: before:top-1/2 before:-translate-y-1/2 before:left-0 before:rounded-full"
+  <div class="flex flex-col gap-7">
+    <div v-for="(stop, index) in stopsList" :key="index" class="relative flex items-center">
+      <span
+        class="flex items-center justify-center w-3.5 h-3.5 rounded-full bg-(--primary-orange) text-white text-xs font-bold"
         :class="{
-          'before:bg-white before:border-2 before:border-[#4D4D4D]': index === stopsList.length - 1,
+          'bg-(--primary-white) border-2 border-(--dark-gray)': index === stopsList.length - 1,
+          'bg-(--dark-gray)': index !== 0 && index !== stopsList.length - 1,
         }"
       >
-        {{ stop?.name }}
-      </div>
+        <span class="" v-if="index !== 0 && index !== stopsList.length - 1">
+          {{ index + 1 }}
+        </span>
+      </span>
+      <p class="ml-3">{{ stop?.name }}</p>
       <div
         v-if="index !== stopsList.length - 1"
-        class="absolute top-1/2 h-[1.5px] bg-[#eeeeee] ml-[30px] w-[calc(100%-40px)]"
+        class="absolute bottom-[-50%] h-[1.5px] bg-[#eeeeee] ml-[30px] w-[calc(100%-40px)]"
       ></div>
     </div>
   </div>
