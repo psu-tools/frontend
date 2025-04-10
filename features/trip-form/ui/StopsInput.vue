dae33604e4dc765fa49ec2e20f61c0d5ac077032
sayranovv <sayranoveldar@gmail.com>
2025-04-10 21:19:24 +0500

    #39 Create stops inputs component

---
+++ features/trip-form/ui/StopsInput.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useAddTripFormStore } from '~/stores/addTripForm'
+
+const addTripFormStore = useAddTripFormStore()
+
+const stopsList = ref([{ name: 'Мое местоположение' }, { name: 'Куда поедем?' }])
+
+const onClickButton = () => {
+  stopsList.value.push({ name: 'Новая точка' })
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-7">
+    <div v-for="(stop, index) in stopsList" :key="index" class="relative flex items-center py-0.5">
+      <span
+        class="flex items-center justify-center w-3.5 h-3.5 text-(--primary-white) text-xs font-bold"
+        :class="{
+          'bg-(--primary-orange) rounded-full': index === 0,
+          'bg-(--primary-white) rounded-full border-2 border-(--dark-gray)':
+            index === stopsList.length - 1,
+          'bg-(--dark-gray) rounded-sm ': index !== 0 && index !== stopsList.length - 1,
+        }"
+      >
+        <span v-if="index !== 0 && index !== stopsList.length - 1"> {{ index }}</span>
+      </span>
+      <input
+        type="text"
+        :placeholder="stop?.name"
+        class="ml-3 text-sm outline-none"
+        :class="{ 'text-(--primary-gray)': index === 1 && stopsList.length === 2 }"
+      />
+      <div
+        v-if="index !== stopsList.length"
+        class="absolute bottom-[-60%] h-[1.5px] bg-[#eeeeee] ml-[26px] w-[calc(100%-34px)]"
+      ></div>
+    </div>
+    <div
+      v-if="stopsList.length >= 2"
+      class="relative flex items-center py-0.5"
+      @click="onClickButton"
+    >
+      <span
+        class="flex items-center justify-center w-3.5 h-3.5 text-(--primary-white) font-light bg-(--primary-gray) rounded-full"
+      >
+        +
+      </span>
+      <p class="ml-3 text-sm font-semibold text-(--primary-gray)">Добавить точку</p>
+    </div>
+  </div>
+</template>
