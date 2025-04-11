e55bbe5c04336355468574788ef495e06df085ce
sayranovv <sayranoveldar@gmail.com>
2025-04-11 23:34:14 +0500

    #39 Create trip form store

---
+++ stores/tripForm.ts
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia'
+
+export const useTripFormStore = defineStore('tripForm', () => {
+  const tripName = ref<string>('')
+
+  const tripDate = ref<Date>(new Date())
+
+  const arrivalTime = ref<string>('')
+
+  const reminderTime = ref<string>('')
+
+  const tripPoints = ref<{ name: string }[]>([])
+
+  const setTripName = (name: string) => {
+    tripName.value = name
+  }
+
+  const setTripDate = (date: Date) => {
+    tripDate.value = date
+  }
+
+  const setArrivalTime = (time: string) => {
+    arrivalTime.value = time
+  }
+
+  const setReminderTime = (time: string) => {
+    reminderTime.value = time
+  }
+
+  const addTripPoint = (point: { name: string }) => {
+    tripPoints.value.push(point)
+  }
+
+  const updateTripPoint = (index: number, point: { name: string }) => {
+    if (index >= 0 && index < tripPoints.value.length) {
+      tripPoints.value[index] = point
+    }
+  }
+
+  return {
+    tripName,
+    tripDate,
+    arrivalTime,
+    reminderTime,
+    tripPoints,
+    setTripName,
+    setTripDate,
+    setArrivalTime,
+    setReminderTime,
+    addTripPoint,
+    updateTripPoint,
+  }
+})
