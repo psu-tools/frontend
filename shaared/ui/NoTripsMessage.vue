9534d1295111240f70f88bc87f8d31992622dee3
sayranovv <sayranoveldar@gmail.com>
2025-04-15 14:38:37 +0500

    #42 Create new no trips message

---
+++ shaared/ui/NoTripsMessage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Folder from '~/shaared/illustrations/Folder.vue'
+import AddTripModal from '~/pages/addTripModal.vue'
+
+const isModalOpen = ref<boolean>()
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col items-center justify-center gap-8">
+      <Folder />
+      <div class="space-y-2.5 text-center">
+        <p class="text-(--color-text)/80 font-semibold">Поездок нет</p>
+        <p class="text-(--primary-light-gray) font-semibold">Добавьте новую поездку</p>
+      </div>
+      <button class="py-2.5 px-[30px] rounded-xl bg-(--primary-orange) text-(--primary-white) cursor-pointer" @click="isModalOpen = true">Добавить</button>
+    </div>
+    <AddTripModal v-if="isModalOpen" @close="isModalOpen = false" />
+  </div>
+</template>
