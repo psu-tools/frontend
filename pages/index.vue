352f01255cf7d6732fd3a09e8e28a081b9c05ebb
sayranovv <sayranoveldar@gmail.com>
2025-04-16 09:00:02 +0500

    #42 Add test loader

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,61 @@ import { useTripsStore } from '@/stores/trips'
 
 const tripsStore = useTripsStore()
 
-onMounted(async () => {
-  await tripsStore.fetchTrips()
-  console.log(tripsStore.activeTrips)
-})
+onMounted(async () => await tripsStore.fetchTrips())
 </script>
 
 <template>
-  <div class="relative flex flex-col gap-9">
+  <div class="relative" :class="{ 'h-full': tripsStore.isLoading }">
     <NuxtLink class="absolute top-1 right-2" to="/notifications">
       <IcNotifications />
     </NuxtLink>
-    <ActiveTrips :trips="tripsStore.activeTrips" />
-    <UpcomingTrips :trips="tripsStore.upcomingTrips" />
+
+    <div
+      v-if="tripsStore.isLoading"
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+    >
+      <p class="h-20 w-20">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+          <circle fill="#FF724C" stroke="#FF724C" stroke-width="15" r="15" cx="40" cy="65">
+            <animate
+              attributeName="cy"
+              calcMode="spline"
+              dur="2"
+              values="65;135;65;"
+              keySplines=".5 0 .5 1;.5 0 .5 1"
+              repeatCount="indefinite"
+              begin="-.4"
+            ></animate>
+          </circle>
+          <circle fill="#FF724C" stroke="#FF724C" stroke-width="15" r="15" cx="100" cy="65">
+            <animate
+              attributeName="cy"
+              calcMode="spline"
+              dur="2"
+              values="65;135;65;"
+              keySplines=".5 0 .5 1;.5 0 .5 1"
+              repeatCount="indefinite"
+              begin="-.2"
+            ></animate>
+          </circle>
+          <circle fill="#FF724C" stroke="#FF724C" stroke-width="15" r="15" cx="160" cy="65">
+            <animate
+              attributeName="cy"
+              calcMode="spline"
+              dur="2"
+              values="65;135;65;"
+              keySplines=".5 0 .5 1;.5 0 .5 1"
+              repeatCount="indefinite"
+              begin="0"
+            ></animate>
+          </circle>
+        </svg>
+      </p>
+    </div>
+
+    <div v-else class="flex flex-col gap-9">
+      <ActiveTrips :trips="tripsStore.activeTrips" />
+      <UpcomingTrips :trips="tripsStore.upcomingTrips" />
+    </div>
   </div>
 </template>
