4f6415060e1cf5eeef0817cc37032c4e9444573f
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-27 20:21:04 +0500

    #45 Add profile info widget

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProfileGeneral from '~/widgets/profileInfo/ProfileGeneral.vue'
+</script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold text-text dark:text-(--primary-white)">Профиль</h1>
+    <h1 class="text-3xl font-bold text-text dark:text-(--primary-white) mb-[25px]">Профиль</h1>
+    <div class="">
+      <ProfileGeneral />
+    </div>
   </div>
 </template>
-
-<style scoped></style>
---
+++ widgets/profileInfo/ProfileGeneral.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IcArrow from '~/icons/IcArrow.vue'
+</script>
+<template>
+  <div
+    class="bg-(--primary-white-bg) dark:bg-(--secondary-black-bg) rounded-(--radius-4xl) py-[15px] pl-[15px] pr-[22px]"
+  >
+    <div class="flex items-center gap-[15px]">
+      <div class="w-[60px] h-[60px] rounded-full bg-(--medium-gray)"></div>
+      <div class="flex-1">
+        <div class="text-(--color-text) dark:text-(--primary-white)">name</div>
+        <div class="text-[#A3A5A8]">email</div>
+      </div>
+      <button class="cursor-pointer">
+        <IcArrow />
+      </button>
+    </div>
+  </div>
+</template>
