5f536082ffb2b751bca1023d1412e976c70826c3
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-27 20:47:58 +0500

    #45 Add my adresses widget

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import ProfileGeneral from '~/widgets/profileInfo/ProfileGeneral.vue'
+import ProfileAdresses from '~/widgets/profileInfo/ProfileAdresses.vue'
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold text-text dark:text-(--primary-white) mb-[25px]">Профиль</h1>
-    <div class="">
+    <div class="flex flex-col gap-[25px]">
       <ProfileGeneral />
+      <ProfileAdresses />
     </div>
   </div>
 </template>
---
+++ widgets/profileInfo/ProfileAdresses.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import IcArrow from '~/icons/IcArrow.vue'
+import IcLocation from '~/icons/profile-widgets/IcLocation.vue'
+</script>
+<template>
+  <div
+    class="bg-(--primary-white-bg) dark:bg-(--secondary-black-bg) rounded-(--radius-2xl) py-[10px] pl-[10px] pr-[20px]"
+  >
+    <div class="flex items-center gap-[10px]">
+      <div>
+        <IcLocation />
+      </div>
+      <div class="flex-1">
+        <div class="text-(--color-text) dark:text-(--primary-white)">Мои адреса</div>
+      </div>
+      <button class="cursor-pointer">
+        <IcArrow />
+      </button>
+    </div>
+  </div>
+</template>
