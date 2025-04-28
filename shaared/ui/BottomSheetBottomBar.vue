955d4eef4dcea4a3e61fa003ecff14d68f564269
sayranovv <sayranoveldar@gmail.com>
2025-04-28 11:23:04 +0500

    #59 Create separate Bottom Bar

---
+++ shaared/ui/BottomSheetBottomBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="absolute bottom-0 left-0 bg-(--primary-white-bg)/40 dark:bg-(--primary-black-bg)/40 backdrop-blur-2xl w-full border-t border-(--medium-gray) dark:border-(--third-black-bg) pt-2.5 pb-10 px-5 flex justify-center items-center"
+  >
+    <slot />
+  </div>
+</template>
