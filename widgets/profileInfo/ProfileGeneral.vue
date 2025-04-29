b60e16ef02ee2760de447202e8d099ee7354568e
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-29 22:15:18 +0500

    #45 Update text width

--- widgets/profileInfo/ProfileGeneral.vue
+++ widgets/profileInfo/ProfileGeneral.vue
@@ -8,8 +8,10 @@ import IcArrow from '~/icons/IcArrow.vue'
     <div class="flex items-center gap-[15px]">
       <div class="w-[60px] h-[60px] rounded-full bg-(--medium-gray)"></div>
       <div class="flex-1">
-        <div class="text-(--color-text) dark:text-(--primary-white) text-sm">name</div>
-        <div class="text-[#A3A5A8] text-xs">email</div>
+        <div class="text-(--color-text) dark:text-(--primary-white) text-sm font-semibold">
+          name
+        </div>
+        <div class="text-[#A3A5A8] text-xs font-semibold">email</div>
       </div>
       <button class="cursor-pointer">
         <IcArrow />
