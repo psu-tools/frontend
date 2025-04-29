d67a19bf88ce9e9e53c066707e0fb8b66568f3a6
Ilya Alikin <iljuha.alikin@gmail.com>
2025-04-29 20:44:47 +0500

    #45 Change logic of block rendering

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import ProfileGeneral from '~/widgets/profileInfo/ProfileGeneral.vue'
-import ProfileRow from '~/widgets/profileInfo/ProfileRow.vue'
 import ProfileBlock from '~/widgets/profileInfo/ProfileBlock.vue'
+
+import IcLocation from '~/icons/profile-widgets/IcLocation.vue'
+
+const block1 = [{ iconComponent: IcLocation, title: 'Мои адреса' }]
+
+const block2: any = [
+  { iconComponent: IcLocation, title: 'Уведомления' },
+  { iconComponent: IcLocation, title: 'Язык' },
+  { iconComponent: IcLocation, title: 'Тема оформления' },
+  { iconComponent: IcLocation, title: 'Запас времени' },
+]
+
+const block3: any = [
+  { iconComponent: IcLocation, title: 'О приложении' },
+  { iconComponent: IcLocation, title: 'Поддержка и обратная' },
+]
 </script>
 
 <template>
@@ -9,8 +24,9 @@ import ProfileBlock from '~/widgets/profileInfo/ProfileBlock.vue'
     <h1 class="text-3xl font-bold text-text dark:text-(--primary-white) mb-[25px]">Профиль</h1>
     <div class="flex flex-col gap-[25px]">
       <ProfileGeneral />
-      <ProfileRow />
-      <ProfileBlock />
+      <ProfileBlock :items="block1" />
+      <ProfileBlock :items="block2" />
+      <ProfileBlock :items="block3" />
     </div>
   </div>
 </template>
--- widgets/profileInfo/ProfileBlock.vue
+++ widgets/profileInfo/ProfileBlock.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import type { Component } from 'vue'
 import ProfileRow from '~/widgets/profileInfo/ProfileRow.vue'
 
-import IcLocation from '~/icons/profile-widgets/IcLocation.vue'
+interface BlockItem {
+  iconComponent: Component
+  title: string
+}
+
+defineProps<{
+  items: BlockItem[]
+}>()
 </script>
 
 <template>
   <div class="bg-(--primary-white-bg) dark:bg-(--secondary-black-bg) rounded-(--radius-2xl)">
     <div class="flex flex-col items-start">
       <div class="w-full">
-        <ProfileRow :icon-component="IcLocation" title="Мои адреса" />
+        <ProfileRow
+          v-for="(item, index) in items"
+          :key="index"
+          :icon-component="item.iconComponent"
+          :title="item.title"
+        />
       </div>
     </div>
   </div>
