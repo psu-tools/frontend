931dd1a289cc3b4cceb466707ce900321debf533
sayranovv <sayranoveldar@gmail.com>
2025-05-01 20:22:59 +0500

    #71 Fix long text display

--- shaared/ui/AddressItem.vue
+++ shaared/ui/AddressItem.vue
@@ -10,11 +10,21 @@ defineProps<{
 </script>
 
 <template>
-  <IcAddressPoint v-if="icon === 'point'" />
-  <IcAddressRecently v-if="icon === 'clock'" />
-  <div class="flex flex-col items-start gap-1">
-    <p class="text-sm text-(--color-text) dark:text-(--primary-white)">{{ name }}</p>
-    <p class="text-xs text-(--primary-gray)">{{ address }}</p>
+  <div class="flex gap-2 items-center justify-start">
+    <div class="w-1/12">
+      <IcAddressPoint v-if="icon === 'point'" />
+      <IcAddressRecently v-if="icon === 'clock'" />
+    </div>
+    <div class="flex flex-col items-start gap-1 w-11/12 overflow-y-hidden">
+      <p
+        class="text-sm text-(--color-text) dark:text-(--primary-white) max-w-full truncate whitespace-nowrap"
+      >
+        {{ name }}
+      </p>
+      <p class="text-xs max-w-full text-(--primary-gray) truncate whitespace-nowrap">
+        {{ address }}
+      </p>
+    </div>
   </div>
 </template>
 
