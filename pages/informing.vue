db9d5902dee668bb94d8dae60a820a39ecff7a06
Ilya Alikin <iljuha.alikin@gmail.com>
2025-05-07 00:05:55 +0500

    #68 Finish informing page

--- pages/informing.vue
+++ pages/informing.vue
@@ -7,13 +7,21 @@ import GeneralWidget from '~/widgets/profilePages/GeneralWidget.vue'
 <template>
   <div class="pb-24">
     <NuxtLink to="/profile" class="w-full">
-      <div class="py-[14px] flex gap-2 items-center">
+      <div class="py-[14px] mb-[10px] flex gap-[10px] items-center">
         <IcBack class="cursor-pointer" />
         <h2 class="text-lg dark:text-(--primary-white) font-semibold">Уведомления</h2>
       </div>
     </NuxtLink>
-    <div class="">
-      <GeneralWidget type="Telegram" id="@username" :has-checkbox="true" />
+    <div class="flex flex-col gap-[15px]">
+      <div class="">
+        <GeneralWidget type="Telegram" id="@username" :has-checkbox="true" />
+      </div>
+      <div class="">
+        <GeneralWidget type="Email" id="email@gmail.com" :has-checkbox="true" />
+      </div>
+      <div class="">
+        <GeneralWidget type="Телефон" id="Функция находится в разработке" :has-checkbox="false" />
+      </div>
     </div>
   </div>
 </template>
