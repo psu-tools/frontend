be5e38381dc2f09cc2d4009380244f9aa5fcdbe3
Ilya Alikin <iljuha.alikin@gmail.com>
2025-05-13 18:51:23 +0500

    #68 Add time page

---
+++ pages/time.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import PagesTitle from '~/widgets/profilePages/PagesTitle.vue'
+import GeneralWidget from '~/widgets/profilePages/GeneralWidget.vue'
+
+import IcClock from '~/icons/IcClock.vue'
+</script>
+
+<template>
+  <div class="pb-24">
+    <div class="mb-[50px]">
+      <PagesTitle title="Запас времени" />
+    </div>
+    <div>
+      <div class="w-full flex justify-center items-center mb-[15px]">
+        <IcClock />
+      </div>
+      <h3
+        class="text-sm text-(--color-text) dark:text-(--primary-white) font-semibold text-center mb-[10px]"
+      >
+        Запас времени
+      </h3>
+      <p class="text-xs text-center mb-[20px] text-(--primary-gray)">
+        Дополнительный процент времени, чтобы учесть пробки, задержки и другие непредвиденные
+        ситуации
+      </p>
+      <div class="flex items-center justify-center gap-[20px]"></div>
+    </div>
+  </div>
+</template>
