92b75d0ccb3aa212af02d71ba665070d4ef6442c
sayranovv <sayranoveldar@gmail.com>
2025-05-15 18:38:19 +0500

    #73 Add app icon

---
+++ icons/IcApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="60" height="60" rx="15" fill="url(#paint0_linear_2500_22239)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear_2500_22239"
+        x1="0"
+        y1="0"
+        x2="0"
+        y2="60"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#FFA181" />
+        <stop offset="1" stop-color="#FF724C" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
