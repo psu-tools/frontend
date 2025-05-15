b269ed49602c471d058b53b8d767523bfdf9c59d
sayranovv <sayranoveldar@gmail.com>
2025-05-15 18:38:04 +0500

    #73 Create new layout for login pages

---
+++ layouts/empty.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="relative bg-(--primary-white) dark:bg-(--primary-black-bg) sm:w-96 h-dvh sm:h-[700px] flex flex-col overflow-hidden p-5"
+  >
+    <NuxtPage />
+    <div id="modal"></div>
+  </div>
+</template>
