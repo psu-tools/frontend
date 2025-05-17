42b75e140ddce8d48b04c66169d0c5895d475a56
sayranovv <sayranoveldar@gmail.com>
2025-05-17 20:33:37 +0500

    #73 Add temp modal

--- pages/registration.vue
+++ pages/registration.vue
@@ -2,13 +2,20 @@
 import EmailStep from '~/features/registration/ui/EmailStep.vue'
 import PasswordStep from '~/features/registration/ui/PasswordStep.vue'
 import InfoStep from '~/features/registration/ui/InfoStep.vue'
+import { useAuthStore } from '~/stores/auth'
+import ErrorModal from '~/features/ErrorModal.vue'
 
 definePageMeta({ layout: 'empty' })
 
+const authStore = useAuthStore()
+
 const step = ref<1 | 2 | 3>(1)
+const isFormSent = ref<boolean>(false)
 
 const nextStep = () => step.value++
 const prevStep = () => step.value--
+
+const sendForm = () => (isFormSent.value = true)
 </script>
 
 <template>
@@ -19,6 +26,12 @@ const prevStep = () => step.value--
     <PasswordStep @next-step="nextStep" @prev-step="prevStep" />
   </div>
   <div v-if="step === 3" class="h-full">
-    <InfoStep @next-step="nextStep" @prev-step="prevStep" />
+    <InfoStep @send-form="sendForm" @prev-step="prevStep" />
   </div>
+  <ErrorModal
+    :is-open="isFormSent"
+    :message="'Добро пожаловать, ' + authStore.name + ' ' + authStore.surname + '!'"
+    @on-click="isFormSent = false"
+    button-text="окак"
+  />
 </template>
