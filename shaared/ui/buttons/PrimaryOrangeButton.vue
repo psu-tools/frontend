ccb549de39d7c1d243baecae3c2206883286d136
sayranovv <sayranoveldar@gmail.com>
2025-05-17 14:16:15 +0500

    #73 Add disabled prop

--- shaared/ui/buttons/PrimaryOrangeButton.vue
+++ shaared/ui/buttons/PrimaryOrangeButton.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ disabled?: boolean }>()
+</script>
 
 <template>
   <button
-    class="bg-(--primary-orange) hover:bg-(--primary-orange-hover) transition-colors text-(--primary-white-bg) rounded-(--radius-2xl) w-full disabled:bg-(--primary-orange-disabled) cursor-pointer"
+    class="bg-(--primary-orange) hover:bg-(--primary-orange-hover) transition-colors text-(--primary-white-bg) rounded-(--radius-2xl) w-full disabled:bg-(--primary-orange-disabled) cursor-pointer disabled:cursor-not-allowed"
+    :disabled="disabled"
   >
     <slot />
   </button>
