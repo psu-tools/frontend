f4c0e1be60cfd5ba4d262b7c0726243f22e699f0
sayranovv <sayranoveldar@gmail.com>
2025-05-18 17:31:05 +0500

    #73 Add dark theme

--- shaared/ui/buttons/SecondaryButton.vue
+++ shaared/ui/buttons/SecondaryButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="bg-(--secondary-button-light) dark:bg-(--third-black-bg) text-(--color-text) dark:text-(--primary-white) rounded-(--radius-2xl) py-[15px] px-10"
+    class="bg-(--secondary-button-light) dark:bg-(--third-black-bg) text-(--color-text) dark:text-(--primary-white) hover:bg-(--primary-white-icon) dark:hover:bg-(--secondary-black-bg-hover) rounded-(--radius-2xl) py-[15px] px-10 transition-colors"
   >
     <slot />
   </button>
