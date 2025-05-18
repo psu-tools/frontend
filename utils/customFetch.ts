e8c2cb410ecb2377cd0d0df74c52589aeb41b2f3
sayranovv <sayranoveldar@gmail.com>
2025-05-18 17:43:07 +0500

    #73 Create custom fetch

---
+++ utils/customFetch.ts
@@ -0,0 +1,35 @@
+export const customFetch = async <T>(url: string, options: any = {}): Promise<T> => {
+  const { accessToken, refreshToken, refresh, logout } = useAuth()
+
+  try {
+    return await $fetch<T>(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${accessToken.value}`,
+      },
+    })
+  } catch (error: any) {
+    if (error?.response?.status === 401) {
+      try {
+        const refreshed = await refresh()
+
+        if (refreshed) {
+          return await $fetch<T>(url, {
+            ...options,
+            headers: {
+              ...options.headers,
+              Authorization: `Bearer ${accessToken.value}`,
+            },
+          })
+        } else {
+          logout()
+          throw new Error('Session expired')
+        }
+      } catch (refreshError) {
+        console.error('Refresh token error', refreshError)
+      }
+    }
+    throw error
+  }
+}
