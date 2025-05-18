35471bfca27e920d9d6fcd380c33d4f2a8cf4610
sayranovv <sayranoveldar@gmail.com>
2025-05-18 17:31:47 +0500

    #73 Add trips fetching

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@ import IcNotifications from '~/icons/IcNotifications.vue'
 import { useTripsStore } from '@/stores/trips'
 
 const tripsStore = useTripsStore()
+
+onMounted(async () => await tripsStore.fetchTrips())
 </script>
 
 <template>
