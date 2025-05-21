4177d4e73855e7cb9a8f94fbcfb689e0ec5bc41e
sayranovv <sayranoveldar@gmail.com>
2025-05-21 10:28:02 +0500

    #84 Add emit

--- widgets/profilePages/PagesTitle.vue
+++ widgets/profilePages/PagesTitle.vue
@@ -8,6 +8,10 @@ interface Props {
 
 const props = defineProps<Props>()
 
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
 let setLink = ''
 if (props.link) {
   setLink = props.link
@@ -18,7 +22,7 @@ if (props.link) {
 <template>
   <NuxtLink :to="`${setLink}`" class="w-full">
     <div class="py-[14px] mb-[10px] flex gap-[10px] items-center">
-      <IcBack class="cursor-pointer" />
+      <IcBack class="cursor-pointer" @click="emit('close')" />
       <h2 class="text-lg dark:text-(--primary-white) font-semibold">{{ title }}</h2>
     </div>
   </NuxtLink>
