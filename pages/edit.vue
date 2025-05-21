c9c54c9cde3a8331626509188920639a8a339367
sayranovv <sayranoveldar@gmail.com>
2025-05-21 10:27:52 +0500

    #84 Edit logic + simplifying

--- pages/edit.vue
+++ pages/edit.vue
@@ -2,30 +2,29 @@
 import PagesTitle from '~/widgets/profilePages/PagesTitle.vue'
 import ProfileRow from '~/widgets/ProfileEdit/ProfileRow.vue'
 import EditProfileField from '~/features/edit-profile/EditProfileField.vue'
-
 import { useUserInfo } from '~/stores/userInfo'
 
-const { getUserInfo } = useUserInfo()
+const userInfoStore = useUserInfo()
 
 import IcUser from '~/icons/IcUser.vue'
-
-interface UserInfo {
-  id: string
-  firstName: string
-  lastName: string
-  email: string
-  phoneNumber: string
-  telegramId: number
-  avatarUri: string
-  userPreferences: object
-}
-
-const userInfo = reactive<Partial<UserInfo>>({})
+import EditFieldModal from '~/pages/editFieldModal.vue'
 
 const isModalOpen = ref(false)
 const editingField = ref<'firstName' | 'lastName' | null>(null)
 const editingLabel = ref('')
 const editingValue = ref('')
+
+const isEmailModalOpen = ref(false)
+const isPhoneModalOpen = ref(false)
+const isTelegramModalOpen = ref(false)
+
+const closeEmailModal = () => (isEmailModalOpen.value = false)
+const closePhoneModal = () => (isPhoneModalOpen.value = false)
+const closeTelegramModal = () => (isTelegramModalOpen.value = false)
+
+const openEmailModal = () => (isEmailModalOpen.value = true)
+const openPhoneModal = () => (isPhoneModalOpen.value = true)
+const openTelegramModal = () => (isTelegramModalOpen.value = true)
 
 const openModal = (field: 'firstName' | 'lastName', label: string, value: string) => {
   editingField.value = field
@@ -35,8 +34,9 @@ const openModal = (field: 'firstName' | 'lastName', label: string, value: string
 }
 
 onMounted(async () => {
-  const response = await getUserInfo()
-  Object.assign(userInfo, response)
+  if (!userInfoStore.userInfo) {
+    await userInfoStore.getUserInfo()
+  }
 })
 
 definePageMeta({
@@ -45,7 +45,7 @@ definePageMeta({
 
 const handleModalSave = (newValue: string) => {
   if (editingField.value) {
-    userInfo[editingField.value] = newValue
+    userInfoStore.userInfo[editingField.value] = newValue
   }
   isModalOpen.value = false
 }
@@ -57,9 +57,9 @@ const handleModalSave = (newValue: string) => {
     <div class="w-full flex items-center justify-center mb-[35px]">
       <div class="flex flex-col items-center gap-[10px]">
         <div class="w-[60px] h-[60px] rounded-full">
-          <div v-if="userInfo.avatarUri">
+          <div v-if="userInfoStore.userInfo?.avatarUri">
             <img
-              :src="userInfo.avatarUri"
+              :src="userInfoStore.userInfo.avatarUri"
               alt="Аватар"
               class="w-32 h-32 rounded-full object-cover"
             />
@@ -77,8 +77,8 @@ const handleModalSave = (newValue: string) => {
       <div>
         <p class="text-xs text-(--color-text-glay) dark:text-(--secondary-gray) mb-[15px]">Имя</p>
         <ProfileRow
-          :label="`${userInfo.firstName}`"
-          @click="openModal('firstName', 'Ваше имя', userInfo.firstName || '')"
+          :label="`${userInfoStore.userInfo?.firstName}`"
+          @click="openModal('firstName', 'Ваше имя', userInfoStore.userInfo?.firstName || '')"
         />
       </div>
       <div>
@@ -86,8 +86,8 @@ const handleModalSave = (newValue: string) => {
           Фамилия
         </p>
         <ProfileRow
-          :label="`${userInfo.lastName}`"
-          @click="openModal('lastName', 'Ваша фамилия', userInfo.lastName || '')"
+          :label="`${userInfoStore.userInfo?.lastName}`"
+          @click="openModal('lastName', 'Ваша фамилия', userInfoStore.userInfo?.lastName || '')"
         />
       </div>
       <div>
@@ -96,19 +96,54 @@ const handleModalSave = (newValue: string) => {
         </p>
         <div class="flex flex-col gap-[15px]">
           <ProfileRow
-            :label="`Телефон`"
-            :value="`${userInfo.phoneNumber}`"
-            :to="`/editing/phoneNumber`"
+            label="Телефон"
+            :value="`${userInfoStore.userInfo?.phoneNumber}`"
+            @click="openPhoneModal"
           />
-          <ProfileRow :label="`Email`" :value="`${userInfo.email}`" :to="`/editing/email`" />
           <ProfileRow
-            :label="`Telegram`"
-            :value="`${userInfo.telegramId}`"
-            :to="`/editing/telegramId`"
+            label="Email"
+            :value="`${userInfoStore.userInfo?.email}`"
+            @click="openEmailModal"
+          />
+          <ProfileRow
+            label="Telegram"
+            :value="`${userInfoStore.userInfo?.telegramId}`"
+            @click="openTelegramModal"
           />
         </div>
       </div>
     </div>
+
+    <Transition name="fade">
+      <EditFieldModal
+        v-if="isEmailModalOpen"
+        @close="closeEmailModal"
+        field="email"
+        :value="userInfoStore.userInfo?.email"
+        class="absolute inset-0 w-full h-full z-50"
+      />
+    </Transition>
+
+    <Transition name="fade">
+      <EditFieldModal
+        v-if="isPhoneModalOpen"
+        @close="closePhoneModal"
+        field="phoneNumber"
+        :value="userInfoStore.userInfo?.phoneNumber"
+        class="absolute inset-0 w-full h-full z-50"
+      />
+    </Transition>
+
+    <Transition name="fade">
+      <EditFieldModal
+        v-if="isTelegramModalOpen"
+        @close="closeTelegramModal"
+        field="telegramId"
+        :value="userInfoStore.userInfo?.telegramId"
+        class="absolute inset-0 w-full h-full z-50"
+      />
+    </Transition>
+
     <EditProfileField
       v-if="isModalOpen"
       :label="editingLabel"
@@ -118,3 +153,18 @@ const handleModalSave = (newValue: string) => {
     />
   </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
