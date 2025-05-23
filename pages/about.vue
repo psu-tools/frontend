330a543b72e9570f36439043270f9f6d63621d8a
Ilya Alikin <iljuha.alikin@gmail.com>
2025-05-24 01:30:25 +0500

    #90 Add phone image

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
+import Phone from '~/assets/img/phone.png'
 definePageMeta({
   layout: 'about',
 })
 </script>
 <template>
-  <div class="w-full px-[256px] pt-[145px] h-[100%]">
-    <div class="flex items">
-      <div class="">
-        <div class="">
+  <div class="w-full px-[256px] pt-[145px] min-h-screen flex items-stretch">
+    <div class="flex justify-between items-start w-full">
+      <div class="flex flex-col justify-between flex-1 pb-[145px]">
+        <div>
           <div class="mb-[60px] text-6xl font-bold">Флоу</div>
           <div class="text-4xl leading-[140%]">
             Твой удобный помощник для <br />
-            эффективного планирования <br />
-            поездок
+            эффективного планирования <br />Phone поездок
           </div>
         </div>
-        <div class="">PSU Tools</div>
+        <div>PSU Tools</div>
       </div>
-      <div class="">afadsfd</div>
+      <div class="flex flex-1 self-end justify-end">
+        <img :src="Phone" alt="" />
+      </div>
     </div>
   </div>
 </template>
